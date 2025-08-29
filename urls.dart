f00f76c40bd05e0c32de0134b0d33a7d4e497c70
Abu Sayeed <asayeedrasel@gmail.com>
2025-08-29 20:12:20 +0600

    Add files via upload

--- urls.dart
+++ urls.dart
@@ -1,10 +1,11 @@
 class Urls {
   static const String _baseUrl = 'http://35.73.30.144:2008/api/v1';
 
-  static const String postProductsUrl = '$_baseUrl/CreateProduct/';
-  static String updateProductsUrl(String id) => '$_baseUrl/UpdateProduct/$id';
+  static const String postProductsUrl = '$_baseUrl/CreateProduct';
 
   static const String getProductsUrl = '$_baseUrl/ReadProduct';
+
+  static String updateProductsUrl(String id) => '$_baseUrl/UpdateProduct/$id';
 
   static String deleteProductsUrl(String id) => '$_baseUrl/DeleteProduct/$id';
 }
